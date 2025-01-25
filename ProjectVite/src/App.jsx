de85c9aa7eef849dc31c989678cac81a6c3964bf
farhan <168018362+FARHAN2324J@users.noreply.github.com>
2025-01-25 21:45:09 +0330

    Add files via upload

---
+++ ProjectVite/src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+      <div className='text-white bg-red-500 w-100 p-5 rounded-lg flex justify-between'>
+        <h1>hi</h1>
+        <h1>hey</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
