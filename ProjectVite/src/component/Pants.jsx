d82aaac2f84da1faf38a099c3cfc129ba27baf7c
farhan <farhan@gmail.com>
2025-02-08 17:18:09 +0330

    up-to-date

--- ProjectVite/src/component/Pants.jsx
+++ ProjectVite/src/component/Pants.jsx
@@ -1,9 +1,18 @@
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
-
+import { useState } from 'react';
 
 
 export default function Pants() {
+    const [items] = useState([
+      {id: 1, image: '/src/imgPants/6.jpg', title: 'Tailored Stretch', description: 'Turn It Up Pants', price: '$180'},
+      {id: 2, image: '/src/imgPants/2.jpg', title: 'Tailored Stretch', description: 'Turn It Up Pants', price: '$150'},
+      {id: 3, image: '/src/imgPants/3.jpg', title: 'Tailored Stretch', description: 'Turn It Up Pants', price: '$130'},
+      {id: 4, image: '/src/imgPants/4.jpg', title: 'Tailored Stretch', description: 'Turn It Up Pants', price: '$140'},
+      {id: 5, image: '/src/imgPants/5.jpg', title: 'Tailored Stretch', description: 'Turn It Up Pants', price: '$160'},
+      {id: 6, image: '/src/imgPants/1.jpg', title: 'Tailored Stretch', description: 'Turn It Up Pants', price: '$200'},
+
+    ])
     return (
         <div>
             <div className='flex  px-10 py-5 gap-5'><Link to="/"><span className='text-gray-700'>Home</span></Link> <span className='text-gray-700'>/</span> <Link><span className='text-gray-950'>Pants</span></Link></div>
@@ -13,102 +22,25 @@ export default function Pants() {
           <span className='text-sm text-[#5A6D57] cursor-pointer'>View All</span>
         </div>
         <div className='flex gap-8 justify-between py-5  flex-wrap'>
-        <div className='w-[30%]'>
-          <div className='relative'>
-            
-            <img src="/src/imgPants/6.jpg" alt="" />
+          {items.map(item => (
+            <div key={item.id} className='w-[30%]'>
+            <div className='relative'>
+              
+              <img src={item.image} alt="" />
+            </div>
+            <h1 className='py-2 font-bold text-base'>{item.title}</h1>
+            <div className='flex justify-between my-2'>
+              <span className='font-normal text-base'>{item.description}</span>
+              <span className='font-bold'>{item.price}</span>
+            </div>
+            <div className='flex gap-2'>
+              <span className='cursor-pointer w-5 h-5 bg-[#0C0C0C] rounded-[50%]'></span>
+              <span className='cursor-pointer w-5 h-5 bg-[#7DC3EB] rounded-[50%]'></span>
+              <span className='cursor-pointer w-5 h-5 bg-[#748C70] rounded-[50%]'></span>
+            </div>
           </div>
-          <h1 className='py-2 font-bold text-base'>Tailored Stretch</h1>
-          <div className='flex justify-between my-2'>
-            <span className='font-normal text-base'>Turn It Up Pants</span>
-            <span className='font-bold'>$180</span>
-          </div>
-          <div className='flex gap-2'>
-            <span className='cursor-pointer w-5 h-5 bg-[#0C0C0C] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#7DC3EB] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#748C70] rounded-[50%]'></span>
-          </div>
-        </div>
-        <div className='w-[30%]'>
-          <div className='relative'>
-            
-            <img src="/src/imgPants/2.jpg" alt="" />
-          </div>
-          <h1 className='py-2 font-bold text-base'>Technical Silk</h1>
-          <div className='flex justify-between my-2'>
-            <span className='font-normal text-base'>Make A Splash</span>
-            <span className='font-bold'>$120</span>
-          </div>
-          <div className='flex gap-2'>
-            <span className='cursor-pointer w-5 h-5 bg-[#909225] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#19418E] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#0C0C0C] rounded-[50%]'></span>
-          </div>
-        </div>
-        <div className='w-[30%]'>
-          <div className='relative'>
-          
-            <img src="/src/imgPants/3.jpg" alt="" />
-          </div>
-          <h1 className='py-2 font-bold text-base'>Cool Weave</h1>
-          <div className='flex justify-between my-2'>
-            <span className='font-normal text-base'>Anywhere Dress</span>
-            <span className='font-bold'>$210</span>
-          </div>
-          <div className='flex gap-2'>
-            <span className='cursor-pointer w-5 h-5 bg-[#D0A5EA] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#909225] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#748C70] rounded-[50%]'></span>
-          </div>
-        </div>
-        <div className='w-[30%]'>
-          <div className='relative'>
-          
-            <img src="/src/imgPants/4.jpg" alt="" />
-          </div>
-          <h1 className='py-2 font-bold text-base'>Cool Weave</h1>
-          <div className='flex justify-between my-2'>
-            <span className='font-normal text-base'>Anywhere Dress</span>
-            <span className='font-bold'>$210</span>
-          </div>
-          <div className='flex gap-2'>
-            <span className='cursor-pointer w-5 h-5 bg-[#D0A5EA] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#909225] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#748C70] rounded-[50%]'></span>
-          </div>
-        </div>
-        <div className='w-[30%]'>
-          <div className='relative'>
-          
-            <img src="/src/imgPants/5.jpg" alt="" />
-          </div>
-          <h1 className='py-2 font-bold text-base'>Cool Weave</h1>
-          <div className='flex justify-between my-2'>
-            <span className='font-normal text-base'>Anywhere Dress</span>
-            <span className='font-bold'>$210</span>
-          </div>
-          <div className='flex gap-2'>
-            <span className='cursor-pointer w-5 h-5 bg-[#D0A5EA] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#909225] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#748C70] rounded-[50%]'></span>
-          </div>
-        </div>
-        <div className='w-[30%]'>
-          <div className='relative'>
-          
-            <img src="/src/imgPants/1.jpg" alt="" />
-          </div>
-          <h1 className='py-2 font-bold text-base'>Cool Weave</h1>
-          <div className='flex justify-between my-2'>
-            <span className='font-normal text-base'>Anywhere Dress</span>
-            <span className='font-bold'>$210</span>
-          </div>
-          <div className='flex gap-2'>
-            <span className='cursor-pointer w-5 h-5 bg-[#D0A5EA] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#909225] rounded-[50%]'></span>
-            <span className='cursor-pointer w-5 h-5 bg-[#748C70] rounded-[50%]'></span>
-          </div>
-        </div>
+          ))}
+        
         </div>
         
         
